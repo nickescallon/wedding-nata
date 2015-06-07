04f33e81ff35955fbf07b23da2471c20d659b23c
Nick Escallon <nickescallon@gmail.com>
2015-06-07 13:44:50 -0400

    bootstrappified

--- app/app.js
+++ app/app.js
@@ -4,36 +4,51 @@ $(document).ready(function() {
     main: $('.container-main'),
     landing: $('#landing'),
     content: {
-      ourWedding: $('#our-wedding'),
-      ourStory: $('#our-story'),
+      itAllStarted: $('#it-all-started'),
+      weddingDay: $('#wedding-day'),
       accommodations: $('#accommodations'),
+      gettingAround: $('#getting-around'),
+      photos: $('#photos'),
       registry: $('#registry')
     },
     nav: {
-      ourWedding: $('#link-our-wedding'),
-      ourStory: $('#link-our-story'),
+      itAllStarted: $('#link-it-all-started'),
+      weddingDay: $('#link-wedding-day'),
       accommodations: $('#link-accommodations'),
-      registry: $('#link-registry')
+      gettingAround: $('#link-getting-around'),
+      photos: $('#link-photos'),
+      registry: $('#link-registry'),
+      collapsed: $('#navbar-collapsed')
     }
   };
 
-  resize();
-  window.onresize = resize;
+  var width, height;
+  setTimeout(resize, 300); // mobile
+  $(window).on('resize orientationChange', resize);
 
   function resize() {
-    // landing page is always 1 window height
-    var landingPageHeight = window.innerHeight - 50;
-    elems.landing.height(landingPageHeight);
+    if($(window).width() != width && $(window).height() != height){
+      // landing page is always 1 window height
+      width = $(window).width(), height = $(window).height();
+      var landingPageHeight = window.innerHeight - 50;
+      elems.landing.height(landingPageHeight);
+      $('.content').css('min-height', landingPageHeight);
+
+    }
   }
 
-  elems.nav.ourWedding.on( 'click', scrollTo.bind(elems.content.ourWedding) );
-  elems.nav.ourStory.on( 'click', scrollTo.bind(elems.content.ourStory) );
+  elems.nav.itAllStarted.on( 'click', scrollTo.bind(elems.content.itAllStarted) );
+  elems.nav.weddingDay.on( 'click', scrollTo.bind(elems.content.weddingDay) );
   elems.nav.accommodations.on( 'click', scrollTo.bind(elems.content.accommodations) );
+  elems.nav.gettingAround.on( 'click', scrollTo.bind(elems.content.gettingAround) );
+  elems.nav.photos.on( 'click', scrollTo.bind(elems.content.photos) );
   elems.nav.registry.on( 'click', scrollTo.bind(elems.content.registry) );
+
 
   function scrollTo() {
     console.log(this, 'clicked')
     var targetTop = this.offset().top - 50;
+    elems.nav.collapsed.collapse('hide');
     elems.body.animate({scrollTop: targetTop}, 750);
   }
 
